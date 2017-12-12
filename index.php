4a15cb83023e76883e3d64bbc33241112e21e187
Yoan Bonnefis <yoan.bonnefis@yahoo.fr>
2017-12-12 14:12:23 +0100

    exercice

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>exo-4</title>
+  </head>
+  <body>
+    <?php
+      $string = "voilà";
+      $int = 12;
+      $float = 12.11;
+      $bool = true;
+      echo $string . ' ' . $int . ' ' . $float . ' ' . $bool;
+    ?>
+  </body>
+</html>
